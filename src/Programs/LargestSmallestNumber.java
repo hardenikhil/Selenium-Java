2c14ab3aeca98e54fb44e74f5b8cced3346b8f8d
nikhil.harde <nikhil.harde@ZIL357.perficient.com>
2019-02-07 19:08:30 +0530

    test

--- src/Programs/LargestSmallestNumber.java
+++ src/Programs/LargestSmallestNumber.java
@@ -9,11 +9,7 @@ public class LargestSmallestNumber {
 		
 		int array[] = {-10, 22, 50, -88, 9867 };//compare -10,22 > 22,50 > 50-88 > -88,9867
 		
-		Arrays.sort(array);//{-88, -10, 22, 50, 9867}
-		System.out.println(Arrays.toString(array));
-		System.out.println("First largest element is "+ array[array.length-1]);
-		
-		/**int largest = array[0];
+		int largest = array[0];
 		int smallest = array[0];
 		
 		for(int i=1; i<array.length; i++ ){
@@ -25,6 +21,6 @@ public class LargestSmallestNumber {
 			}
 		}
 		System.out.println("Largest number is =" + largest);
-		System.out.println("Smallest number is =" + smallest);**/
+		System.out.println("Smallest number is =" + smallest);
 	}
 }
